54e2fc43fd97c209788b908529939281202bae87
luquinha <lucaspirlo55@gmail.com>
2021-02-03 21:58:20 -0300

    [feat]: change main file to render new routes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueDragResize from 'vue-drag-resize'
+import titleMixin from './mixins/titleMixin'
+import store from './store'
 
-
+Vue.mixin(titleMixin)
 Vue.component('vue-drag-resize', VueDragResize)
 
-
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
