f019d15bc749a8a02c07424419a0d6f76f2f7d74
luquinha <lucaspirlo55@gmail.com>
2021-02-03 21:56:43 -0300

    [feat]: move store to src folder/change logics

---
+++ src/store/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import * as pageServices from "../services/pageServices";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    pageLinks: [],
+  },
+  mutations: {
+    SET_PAGE_LINKS(state, pageLinks) {
+      state.pageLinks = pageLinks;
+    },
+    UPDATE_COORDS(state, payload) {
+      let pageLink = state.pageLinks.find(el => el.link=== payload.link)
+      if (pageLink) {
+        pageLink.coords = payload.coords
+      }
+    },
+  },
+  getters: {
+    links: (state) => state.pageLinks,
+    link: (state) => (link) => state.pageLinks.find((page) => page.link === link)
+  },
+  actions: {
+    updateCoords({ commit }, payload) {
+      commit("UPDATE_COORDS", payload);
+    },
+    async getPages({ commit }) {
+      const pageLinks = await pageServices.getPages()
+      commit("SET_PAGE_LINKS", pageLinks)
+    },
+    async postPage({ commit }, payload) {
+      const coords = await pageServices.postPage(payload)
+      commit("UPDATE_COORDS", {
+        link: payload,
+        coords
+      });
+    },
+  },
+});
